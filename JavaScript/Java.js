f78e9da81fd79a66241a8eb39568edf4ca6d1f5c
Guiexx2 <139629718+Guiexx2@users.noreply.github.com>
2023-10-07 09:44:22 -0300

    Testando comando java

---
+++ JavaScript/Java.js
@@ -0,0 +1,8 @@
+let Nome = prompt("Digite seu nome: ")
+let atual = Number(prompt("Em que ano estamos"))
+let nasc = Number(prompt("Em que ano você nasceu"))
+let idade
+alert(`Seu nome é: ${Nome}`)
+alert(`Sua idade é: ${idade = atual - nasc}`)
+alert(`"É maior de idade? ${idade > 17}`)
+alert(2050 - nasc)
